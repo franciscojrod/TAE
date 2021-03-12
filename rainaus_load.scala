3908e68327d2d57e9c2f0410771963fe66a31394
Usuario <usuario@virtual.lab.inf.uva.es>
2021-03-12 22:33:45 +0100

    load data

---
+++ rainaus_load.scala
@@ -0,0 +1,45 @@
+import org.apache.spark.sql.types.{IntegerType, DoubleType, StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+from pyspark.sql.functions import min, max
+
+println("\n\n******************* CARGA DE DATOS *******************\n\n")
+
+val PATH = "/home/usuario/australia/"
+val FILE_WEATHER = "weatherAUS.csv"
+
+
+val weatherSchema = StructType(Array(
+  StructField("Date", StringType, true),
+  StructField("Location", StringType, true),
+  StructField("MinTemp", DoubleType, true),
+  StructField("MaxTemp", DoubleType, true),
+  StructField("RainFall", DoubleType, true),
+  StructField("Evaporation", StringType, true),
+  StructField("Sunshine", StringType, true),
+  StructField("WindGustDir", StringType, true),
+  StructField("WindGustSpeed", StringType, true),
+  StructField("WindDir9am", StringType, true),
+  StructField("WindDir3pm", StringType, true),
+  StructField("WindSpeed9am", IntegerType, true),
+  StructField("WindSpeed3pm", IntegerType, true),
+  StructField("Humidity9am", IntegerType, true),
+  StructField("Humidity3pm", StringType, true),
+  StructField("Pressure9am", DoubleType, true),
+  StructField("Pressure3pm", DoubleType, true),
+  StructField("Cloud9am", IntegerType, true),
+  StructField("Cloud3pm", IntegerType, true),
+  StructField("Temp9am", DoubleType, true),
+  StructField("Temp3pm", DoubleType, true),
+  StructField("RainToday", StringType, true),
+  StructField("RainTomorrow", StringType, true)));
+
+val weatherDF = spark.read.format("csv").
+  option("delimiter", ",").
+  option("header", true).
+  schema(weatherSchema).load(PATH + FILE_WEATHER)
+
+val primero = weatherDF.first()
+println("Primer registro: " + primero)
+
+val num_records = weatherDF.count()
+println("Numero de registros: " + num_records)
