6fc916923b62b49ff72f10a87e0f447177abe2e1
Fran <48751836+franciscojrod@users.noreply.github.com>
2021-03-17 21:58:49 +0100

    Update OneHotEncoderModel.scala

--- OneHotEncoderModel.scala
+++ OneHotEncoderModel.scala
@@ -4,7 +4,7 @@ import org.apache.spark.ml.feature.StringIndexerModel
 
 // Obtenemos el nombrede las columnasde carDF, salvo la clase
 
-val attributeColumns= Seq("Date", "Location", "WindGustDir", "WindDir9am", "WindDir3pm", "RainToday", "RainTomorrow").toArray
+val attributeColumns= Seq("Date", "Location", "WindGustDir", "WindDir9am", "WindDir3pm", "RainToday").toArray
 
 // Generamos los nombres de las nuevas columnas
 val outputColumns = attributeColumns.map(_ + "-num").toArray
@@ -16,12 +16,13 @@ val simColumns= siColumns.fit(weatherDF4_train)
 
 val weatherDFnumeric= simColumns.transform(weatherDF4_train).drop(attributeColumns:_*)
 
+
 // VectorAssembler
 
 import org.apache.spark.ml.feature.OneHotEncoder
 import org.apache.spark.ml.feature.OneHotEncoderModel
 
-val attributeColumns_hot= Seq("Date", "Location", "WindGustDir", "WindDir9am", "WindDir3pm", "RainToday", "RainTomorrow").toArray
+val attributeColumns_hot= Seq("Date", "Location", "WindGustDir", "WindDir9am", "WindDir3pm", "RainToday").toArray
 val inputCol = outputColumns
 // Generamos los nombres de las nuevas columnas
 val outputColumns_hot = attributeColumns_hot.map(_ + "-hot").toArray
@@ -32,12 +33,19 @@ val hotmColumns= hotColumns.fit(weatherDFnumeric)
 
 val WeatherDFhot = hotmColumns.transform(weatherDFnumeric).drop(inputCol:_*)
 
-WeatherDFhot.show(5)
 
 import org.apache.spark.ml.feature.VectorAssembler
 
 val va = new VectorAssembler().setOutputCol("features").setInputCols(outputColumns_hot)
 
-val WeatherFeaturesClaseDF = va.transform(WeatherDFhot).select("features", "RainTomorrow-hot")
+val WeatherFeaturesClaseDF = va.transform(WeatherDFhot).select("features", "RainTomorrow")
 
 WeatherFeaturesClaseDF.show(5)
+
+
+val indiceClase= new StringIndexer().setInputCol("RainTomorrow").setOutputCol("label").setStringOrderType("alphabetDesc")
+val WeatherFeaturesClaseDFLabel = indiceClase.fit(WeatherFeaturesClaseDF).transform(WeatherFeaturesClaseDF).drop("RainTomorrow")
+
+WeatherFeaturesClaseDFLabel.show(5)
+
+
