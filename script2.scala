94386aeee77d67141a49863261e2986b33ee4f07
Fran <48751836+franciscojrod@users.noreply.github.com>
2021-03-21 20:43:20 +0100

    Update script2.scala

--- script2.scala
+++ script2.scala
@@ -18,7 +18,7 @@ println("\n\n******************* CARGA DE DATOS *******************\n\n")
 
 val PATH = "/home/usuario/australia/"
 val FILE_WEATHER = "weatherAUS.csv"
-val MODEL_FOLDER = "model/"
+val MODEL_FOLDER = "modelo/"
 
 val weatherSchema = StructType(Array(
   StructField("Date", StringType, true),
