224c38aa4df77b79692d97d2e1b6d29a6f37959c
Fran <48751836+franciscojrod@users.noreply.github.com>
2021-04-04 18:43:10 +0200

    Update modelo1_script1.scala

--- entrega3/modelo1_script1.scala
+++ entrega3/modelo1_script1.scala
@@ -18,7 +18,10 @@ import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.mllib.evaluation.MultilabelMetrics
-
+import org.apache.spark.ml.classification.RandomForestClassifier
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.tuning.ParamGridBuilder
+import org.apache.spark.ml.tuning.CrossValidator
 
 
 /*
@@ -388,4 +391,67 @@ val metrics = new BinaryClassificationMetrics(lrModelLbFe)
 
 metrics.areaUnderPR()
 
+// random forest
 
+
+val seed = 5043
+val Array(trainingData, testData) = weatherFeaturesLabelDF.randomSplit(Array(0.7, 0.3), seed)
+
+val Array(pipelineTrainingData, pipelineTestingData) = weatherDF4_train.randomSplit(Array(0.7, 0.3), seed)
+val cols1 = Array("MinTemp", "MaxTemp", "Rainfall", "WindGustSpeed", "WindSpeed9am", "WindSpeed3pm", "Pressure9am", "Humidity9am", "Humidity3pm")
+
+
+val assembler = new VectorAssembler()
+  .setInputCols(cols1)
+  .setOutputCol("features")
+val featureDf = assembler.transform(weatherDF4_train)
+featureDf.printSchema()
+
+
+val indexer = new StringIndexer()
+  .setInputCol("RainTomorrow")
+  .setOutputCol("label")
+val labelDf = indexer.fit(featureDf).transform(featureDf)
+labelDf.printSchema()
+
+val randomForestClassifier = new RandomForestClassifier()
+
+val stages = Array(assembler, indexer, randomForestClassifier)
+
+val pipeline_randomForest = new Pipeline().setStages(stages)
+val pipelineModel = pipeline_randomForest.fit(pipelineTrainingData)
+
+// test model with test data
+val pipelinePredictionDf = pipelineModel.transform(pipelineTestingData)
+pipelinePredictionDf.show(10)
+
+// evaluate model with area under ROC
+val evaluator = new BinaryClassificationEvaluator()
+  .setLabelCol("label")
+  .setMetricName("areaUnderROC")
+
+
+// measure the accuracy of pipeline model
+val pipelineAccuracy = evaluator.evaluate(pipelinePredictionDf)
+println(pipelineAccuracy)
+
+
+trainingData.show(5)
+
+
+
+val paramGrid_randomForest = new ParamGridBuilder().addGrid(randomForestClassifier.maxBins, Array(42, 49,  80)).addGrid(randomForestClassifier.maxDepth, Array(3, 6, 10)).addGrid(randomForestClassifier.impurity, Array( "gini", "entropy")).build()
+
+val cv_randomForest = new CrossValidator().setEstimator(pipeline_randomForest).setEvaluator(evaluator).setEstimatorParamMaps(paramGrid).setNumFolds(5)
+
+// this will take some time to run
+val cvModel_randomForest = cv_randomForest.fit(pipelineTrainingData)
+// test cross validated model with test data
+val cvPredictionDf_randomForest = cvModel_randomForest.transform(pipelineTestingData)
+cvPredictionDf_randomForest.show(10)
+
+val cvAccuracy = evaluator.evaluate(cvPredictionDf_randomForest)
+println(cvAccuracy)
+
+val bestModel_randomForest = cvModel_randomForest.bestModel
+println(bestModel_randomForest.asInstanceOf[PipelineModel].stages.last.extractParamMap)
